1c649e0e2aa36b7941eff4765e56c449cda34eaf
Shirsendu Das <37840366+DarkenLight@users.noreply.github.com>
2023-03-08 06:44:07 +0000

    Update and rename config.sh to testservice.sh

--- testservice.sh
+++ testservice.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 project_name="Jellyfinserver"
+echo "Installation is complete"
 
-clear
+
+
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo "       _   ______   _        _       __     __  ______   _____   _   _ "
 echo "      | | |  ____| | |      | |      \ \   / / |  ____| |_   _| | \ | |"
@@ -12,28 +14,5 @@ echo " | |__| | | |____  | |____  | |____     | |    | |       _| |_  | |\  |"
 echo "  \____/  |______| |______| |______|    |_|    |_|      |_____| |_| \_|"
 echo "                                                                       "
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo "                    MANAGED $project_name SCRIPTED INSTALLER                     "
+if service ${project_name} status | grep -q "is running"; then echo "${project_name} is Running at $HOSTNAME:8069"; else echo "${project_name} is not running.."; fi
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo
-read -p "Do you want to Mount the Media Folder now as well?" -n 1 -r
-echo  # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-echo "Enter Remote Directory path: "
-read -p ">>" media_dir
-echo "Enter Local Directory path: "
-read -p ">>" mount_dir
-
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo "Entered Details are Below"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo "MEDIA DIRECTORY = ${media_dir}"
-echo "MOUNTED LOCATION = ${mount_dir}"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-
-sysrc ${project_name}_media_dir="${media_dir}"
-sysrc ${project_name}_mount_dir="${mount_dir}"
-fi
-
-echo "Setup complete"
-echo "${project_name} Running at $HOSTNAME:8069"
